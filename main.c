d6d37a312ba0044d92e1fb7d46058e4a2a5f7e70
Christos Panagiotakopoulos <chrispanag@gmail.com>
2020-12-31 14:11:27 +0200

    Finish up chat

--- main.c
+++ main.c
@@ -1,104 +1,178 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <signal.h>
 #include <stdio.h>
-#include <sys/types.h> /* See NOTES */
-#include <sys/socket.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#include <arpa/inet.h>
-
 #define DEFAULT_TCP_PORT 3000
+#define DEFAULT_HOSTNAME "127.0.0.1"
+#define BUFFER_SIZE 1000
+#define TCP_BACKLOG 1
 
-const int TCP_BACKLOG = 5;
+int client(char *hostname, int port) {
+  int sd;
+  /* Create TCP/IP socket, used as main chat channel */
+  if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    exit(1);
+  }
 
-void handleConnection(int newsd)
-{
-    int n;
-    char buf[100];
-    for (;;)
-    {
-        n = read(newsd, buf, sizeof(buf));
-        if (n <= 0)
-        {
-            if (n < 0)
-                perror("read from remote peer failed");
-            else
-                fprintf(stderr, "Peer went away\n");
-            break;
-        }
-        printf("%s\n", buf);
-    }
+  struct hostent *hp;
+  struct sockaddr_in sa;
+
+  /* Look up remote hostname on DNS */
+  if (!(hp = gethostbyname(hostname))) {
+    printf("DNS lookup failed for host %s\n", hostname);
+    exit(1);
+  }
+
+  /* Connect to remote TCP port */
+  sa.sin_family = AF_INET;
+  sa.sin_port = htons(port);
+  memcpy(&sa.sin_addr.s_addr, hp->h_addr, sizeof(struct in_addr));
+  if (connect(sd, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+    perror("connect");
+    exit(1);
+  }
+
+  return sd;
 }
 
-void server(int port)
-{
-    int sd;
+int server(int port) {
+  int sd;
 
-    if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("socket");
-        exit(1);
-    }
-    fprintf(stderr, "Created TCP socket\n");
+  if ((sd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    exit(1);
+  }
 
-    struct sockaddr_in sa;
+  struct sockaddr_in sa;
 
-    memset(&sa, 0, sizeof(sa));
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(port);
-    sa.sin_addr.s_addr = htonl(INADDR_ANY);
-    if (bind(sd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
-    {
-        perror("bind");
-        exit(1);
-    }
-    fprintf(stderr, "Bound TCP socket to port %d\n", port);
+  memset(&sa, 0, sizeof(sa));
+  sa.sin_family = AF_INET;
+  sa.sin_port = htons(port);
+  sa.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind(sd, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+    perror("bind");
+    exit(1);
+  }
 
-    /* Listen for incoming connections */
-    if (listen(sd, TCP_BACKLOG) < 0)
-    {
-        perror("listen");
-        exit(1);
-    }
+  printf("Waiting for peer...\n");
 
-    socklen_t len;
-    int newsd, n;
-    char addrstr[INET_ADDRSTRLEN], buf[2];
-    for (;;)
-    {
-        fprintf(stderr, "Waiting for an incoming connection...\n");
+  /* Listen for incoming connections */
+  if (listen(sd, TCP_BACKLOG) < 0) {
+    perror("listen");
+    exit(1);
+  }
 
-        /* Accept an incoming connection */
-        len = sizeof(struct sockaddr_in);
-        if ((newsd = accept(sd, (struct sockaddr *)&sa, &len)) < 0)
-        {
-            perror("accept");
-            exit(1);
-        }
-        if (!inet_ntop(AF_INET, &sa.sin_addr, addrstr, sizeof(addrstr)))
-        {
-            perror("could not format IP address");
-            exit(1);
-        }
-        fprintf(stderr, "Incoming connection from %s:%d\n",
-                addrstr, ntohs(sa.sin_port));
+  socklen_t len;
+  int newsd, n;
+  char addrstr[INET_ADDRSTRLEN], buf[2];
+  /* Accept an incoming connection */
+  len = sizeof(struct sockaddr_in);
+  if ((newsd = accept(sd, (struct sockaddr *)&sa, &len)) < 0) {
+    perror("accept");
+    exit(1);
+  }
+  if (!inet_ntop(AF_INET, &sa.sin_addr, addrstr, sizeof(addrstr))) {
+    perror("could not format IP address");
+    exit(1);
+  }
+  printf("Incoming connection from %s:%d\n", addrstr, ntohs(sa.sin_port));
 
-        handleConnection(newsd);
-
-        if (close(newsd) < 0)
-            perror("close");
-    }
+  return newsd;
 }
 
-int main(int argc, char **argv)
-{
-    int port = DEFAULT_TCP_PORT;
-    if (argc > 1)
-    {
-        port = atoi(argv[1]);
+ssize_t insist_write(int fd, const void *buf, size_t cnt) {
+  ssize_t ret;
+  size_t orig_cnt = cnt;
+
+  while (cnt > 0) {
+    ret = write(fd, buf, cnt);
+    if (ret < 0) return ret;
+    buf += ret;
+    cnt -= ret;
+  }
+
+  return orig_cnt;
+}
+
+void handleReceive(int d) {
+  int n;
+  char buf[BUFFER_SIZE];
+  for (;;) {
+    n = read(d, buf, sizeof(buf));
+
+    if (n <= 0) {
+      if (n < 0)
+        perror("\nread from remote peer failed\n");
+      else
+        fprintf(stderr, "\nPeer went away\n");
+      break;
     }
 
-    server(port);
+    // Delete last line, print the message and then print the prompt
+    printf("\rOther guy: %sYou: ", buf);
+    fflush(stdout);
+  }
+}
 
-    return 0;
+void handleSend(int sd) {
+  char readbuf[BUFFER_SIZE];
+  int n;
+
+  for (;;) {
+    fgets(readbuf, BUFFER_SIZE - 1, stdin);
+    if (readbuf[0] == '\n') {
+      continue;
+    }
+    printf("You: ");
+    n = strlen(readbuf);
+    // /* Be careful with buffer overruns, ensure NUL-termination */
+
+    if (insist_write(sd, readbuf, n + 1) != n + 1) {
+      perror("write");
+      exit(1);
+    }
+    readbuf[0] = '\0';
+    fflush(stdout);
+  }
+}
+
+int main(int argc, char **argv) {
+  int listen_port = DEFAULT_TCP_PORT;
+  char *hostname = DEFAULT_HOSTNAME;
+  if (argc < 2 || argc > 4) {
+    printf("Usage: <server|client> <port> <hostname>\n");
+    exit(1);
+  }
+
+  if (argc > 2) {
+    listen_port = atoi(argv[2]);
+  }
+  if (argc == 4) {
+    hostname = argv[3];
+  }
+
+  int d;
+  if (strcmp(argv[1], "server") == 0) {
+    d = server(listen_port);
+  } else {
+    d = client(hostname, listen_port);
+  }
+
+  printf("You: ");
+  fflush(stdout);
+
+  if (fork() == 0) {
+    handleReceive(d);
+  } else {
+    handleSend(d);
+  }
+  if (close(d) < 0) perror("close");
+  return 1;
 }
